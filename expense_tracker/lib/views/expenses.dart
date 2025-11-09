c654c76c3721b4eacee5c1c871db6d73daf7d0e1
Guilherme Reichert <guireichert.tr@gmail.com>
2025-11-09 01:35:27 -0300

    Clear Button and Total Amount

--- expense_tracker/lib/views/expenses.dart
+++ expense_tracker/lib/views/expenses.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:expense_tracker/Custom%20Widgets/new_expense.dart';
 import 'package:expense_tracker/Dummy%20Data/expense_list.dart';
 import 'package:expense_tracker/Models/expense_model.dart';
@@ -13,13 +15,16 @@ class Expenses extends StatefulWidget {
 
 class _ExpensesState extends State<Expenses> {
   final List<ExpenseModel> _registeredExpenses = expensesList;
+  double _totalAmount = 0;
 
   @override
   Widget build(context) {
+    _recalculateTotal();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Expense Tracker'),
-        backgroundColor: Colors.blueAccent[100],
+        backgroundColor: Colors.grey[300],
         actions: [
           IconButton(onPressed: _openAddExpenseOverlay, icon: Icon(Icons.add)),
         ],
@@ -27,15 +32,49 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           Text('Chart will come here'),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                child: ElevatedButton(
+                  onPressed: _removeAllExpenses,
+                  child: Text('Clear'),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
           Expanded(
-            child: ExpensesList(
-              expensesList: _registeredExpenses,
-              removeItem: _removeExpense,
+            child: _registeredExpenses.isEmpty
+                ? noExpensesWidget()
+                : ExpensesList(
+                    expensesList: _registeredExpenses,
+                    removeItem: _removeExpense,
+                  ),
+          ),
+          Spacer(),
+          Card(
+            color: Colors.grey[300],
+            elevation: 200,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 0, vertical: 15),
+              child: Center(
+                child: Text(
+                  'Total: \$ ${_totalAmount.toStringAsFixed(2)}',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  Widget noExpensesWidget() {
+    return Center(child: Text('No expenses found. Start adding'));
   }
 
   void _openAddExpenseOverlay() {
@@ -51,12 +90,50 @@ class _ExpensesState extends State<Expenses> {
   void _addExpense(ExpenseModel expense) {
     setState(() {
       _registeredExpenses.add(expense);
+      _recalculateTotal();
     });
   }
 
   void _removeExpense(ExpenseModel expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Enpense deleted.'),
+        duration: Duration(seconds: 3),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(
+                expenseIndex,
+                expense,
+              ); // Retorna no mesmo lugar
+              _recalculateTotal();
+            });
+          },
+        ),
+      ),
+    );
+    _recalculateTotal();
+  }
+
+  void _removeAllExpenses() {
+    setState(() {
+      _registeredExpenses.clear();
+      _recalculateTotal();
+    });
+  }
+
+  void _recalculateTotal() {
+    _totalAmount = _registeredExpenses.fold(
+      0.0,
+      (sum, item) => sum + item.amount,
+    );
   }
 }
